648f5040bdb96009a17200cd5a46014c2418d2e9
bengrid <71706934+bengrid@users.noreply.github.com>
2021-03-26 11:58:57 +0100

    add

--- src/project/sm/Passerelle.java
+++ src/project/sm/Passerelle.java
@@ -318,25 +318,22 @@ java.sql.Date laDate = new java.sql.Date(uneDate.getTime());
     // COMMANDES PHARMACIE FOURNISSEURS
     public static boolean ajouterCommande(int idM,int qtte) throws SQLException{
         boolean verif = false;
-        /**
-        Commande uneCommande = new Commande(idCommandes,laDate,qtte);// creation d'un objet commande
-        uneCommande.ajouterCommande(uneCommande);
-        **/
         try{
             String prep1 = "insert into commande(idm,qtteCommande,dateCommande) values("+idM+","+qtte+",NOW())"; //ajout de la commande dans la table commande
             Statement state = connexionBdd().createStatement();
             int nb = state.executeUpdate(prep1);
-            verif = true;
-            /**
-            Statement prep2 = connexionBdd().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT qtteStock from Medicaments WHERE idM ='idM' ");
-            int laQtte = res.getInt(1); // recup le resultat
+          
+            String prep2 = ("SELECT qtteStock from Medicament WHERE idm ="+idM+" ");   
+            ResultSet rs =state.executeQuery(prep2);
+            rs.next();           
+            int laQtte = rs.getInt(1); // recup le resultat
             laQtte=laQtte+qtte;
+             
             
-            PreparedStatement prep3 = connexionBdd().prepareStatement("UPDATE Medicament SET qtteStock = 'res' WHERE idM='idM'"); //Ajout de la commande dans le stock pharmacie
-            prep3.setInt(1,laQtte);
+            String prep3 = ("UPDATE Medicament SET qtteStock = "+laQtte+" WHERE idM="+idM+""); //Ajout de la commande dans le stock pharmacie
+            int nb2 = state.executeUpdate(prep3);           
             verif=true;
-           **/
+            
         }catch(SQLException e){
             verif=false;
             e.printStackTrace();
@@ -347,35 +344,36 @@ java.sql.Date laDate = new java.sql.Date(uneDate.getTime());
     }
     
     // envoie de la demande
-    public static void envoyerMedicament(int idMedicament,int nbCommande) throws SQLException{
-        try{
-            Statement stmt = connexionBdd().createStatement();
-            PreparedStatement prep = connexionBdd().prepareStatement("insert into Demande(idD,nbCommande,dateDuJour) values(id,nbC,NOW())"); //Ajout de la demande dans la table demande
-            prep.setInt(1,idMedicament);
-            prep.setInt(2, nbCommande);
-            prep.executeUpdate();
+    public static boolean envoyerMedicament(int idMedicament,int idService,int nbcommande) throws SQLException{
+        boolean verif = false;
+        try{           
+            String prep1 = "insert into Demande(idM,idservice,nbcommand,dateDuJour) values("+idMedicament+","+idService+","+nbcommande+",NOW())"; //Ajout de la demande dans la table demande
+            Statement state = connexionBdd().createStatement();
+            int nb = state.executeUpdate(prep1);   
+            verif=true;
             }catch(SQLException e){
                     e.printStackTrace();
-            }
+            }return(verif);
     }
     
     
     // COMMANDES PHARMACIE FOURNISSEURS
-    public void ajouterCommandeF(int idCommandes,int qtte) throws SQLException{
-        try{
-            Statement stmt = connexionBdd().createStatement();
-            PreparedStatement prep1 = connexionBdd().prepareStatement("insert into Commandes(idCommande,qtteCommande,dateCommande) values(id,qtte,NOW())");
-            prep1.setInt(1,idCommandes);
-            prep1.setInt(2,qtte);
-            prep1.executeUpdate();
+    public boolean ajouterCommandeF(int idCommandes,int qtte) throws SQLException{
+        boolean verif = false;
+        try{     
+            String prep1 =("insert into Commandes(idCommande,qtteCommande,dateCommande) values("+idCommandes+","+qtte+",NOW())");
+            Statement state = connexionBdd().createStatement();
+            int nb = state.executeUpdate(prep1);
             
-            Statement prep2 = connexionBdd().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT qtteStock");
+            String prep2 = ("SELECT qtteStock");
+            ResultSet rs = state.executeQuery(prep2);
+            rs.next();
+            //a finir
             
             PreparedStatement prep3 = connexionBdd().prepareStatement("UPDATE Medicament SET qtteStock = ''");
         }catch(SQLException e){
             e.printStackTrace();
-        }
+        }return (verif);
         
     }
 
